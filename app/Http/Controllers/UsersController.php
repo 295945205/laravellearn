47675de19cf2e6284c17fae0971466dea587cf1b
szh <295945205@qq.com>
2017-07-02 22:22:51 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -26,6 +27,8 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
